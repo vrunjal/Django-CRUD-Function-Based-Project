464ef88b52b427f073bced4dc7657ae12b2eb005
Vrunjal N. Panchal <vrunjal.panchal2907@gmail.com>
2023-10-05 07:59:10 +0530

    admin,form,Model created

---
+++ project_CRUD/app_enroll/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppEnrollConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_enroll'
---
+++ project_CRUD/app_enroll/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
+# Create your views here.
+def add_show(request):
+    if request.method == "POST":
+        fm=StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm=fm.cleaned_data['name']
+            em=fm.cleaned_data['email']
+            pw=fm.cleaned_data['password']
+            
+            reg=User(name=nm,email=em,password=pw)
+            reg.save()
+            fm=StudentRegistration()   
+            
+    else:
+        fm=StudentRegistration()
+    return render(request,'app_enroll/addandshow.html',{'form':fm})
